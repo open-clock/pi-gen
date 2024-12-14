4ae096b45ec83032ac340db30016d07c7b947c54
Your Name <you@example.com>
2024-12-15 00:09:17 +0100

    feat: rough config

---
+++ stage2-openclock/00-branding/01-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+install -m 644 files/issue		"${ROOTFS_DIR}/etc/issue"
+install -m 644 files/issue.net		"${ROOTFS_DIR}/etc/issue.net"
+install -m 644 files/motd		"${ROOTFS_DIR}/etc/motd"
