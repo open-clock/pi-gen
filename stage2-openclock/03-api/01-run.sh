470d790e4929be66ad8162ea35ab1a9c84dfba86
GameTec_live <benedikt.werner08@gmail.com>
2025-01-02 15:40:11 +0100

    feat: api service

--- stage2-openclock/03-api/01-run.sh
+++ stage2-openclock/03-api/01-run.sh
@@ -1,1 +1,16 @@
 #!/bin/bash -e
+mkdir -p "${ROOTFS_DIR}/api"
+cp -r files/api/source/* "${ROOTFS_DIR}/api/"
+chmod -R 644 "${ROOTFS_DIR}/api"
+chmod -R +X "${ROOTFS_DIR}/api"
+
+on_chroot << EOF
+	cd /api
+    pip3 install --no-cache-dir --break-system-packages -r requirements.txt
+EOF
+
+install -m 644 files/api.service "${ROOTFS_DIR}/etc/systemd/system/api.service"
+
+on_chroot << EOF
+    systemctl enable api
+EOF
