bf0d8a263e241e964437d42d08ce2514b76b5483
GameTec_live <benedikt.werner08@gmail.com>
2025-01-24 21:43:52 +0100

    fix: init submodule

--- stage2-openclock/04-displaydriver/01-run.sh
+++ stage2-openclock/04-displaydriver/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 mkdir -p "${ROOTFS_DIR}/epd-lib"
-cp -r files/epd-lib/* "${ROOTFS_DIR}/epd-lib/"
+cp -r files/driver/epd-lib/* "${ROOTFS_DIR}/epd-lib/"
 chmod -R 644 "${ROOTFS_DIR}/epd-lib"
 chmod -R +X "${ROOTFS_DIR}/epd-lib"
 
@@ -16,29 +16,29 @@ EOF
 rm -r "${ROOTFS_DIR}/epd-lib"
 
 mkdir -p "${ROOTFS_DIR}/displaydriver"
-install -m 644 files/bootsplashdriver.py "${ROOTFS_DIR}/displaydriver/bootsplashdriver.py"
-install -m 644 files/shutdowndriver.py "${ROOTFS_DIR}/displaydriver/shutdowndriver.py"
-install -m 644 files/clear.py "${ROOTFS_DIR}/displaydriver/clear.py"
-install -m 644 files/driver.py "${ROOTFS_DIR}/displaydriver/driver.py"
-install -m 644 files/Geist-Regular.ttf "${ROOTFS_DIR}/displaydriver/Geist-Regular.ttf"
-install -m 644 files/GeistMono-Regular.ttf "${ROOTFS_DIR}/displaydriver/GeistMono-Regular.ttf"
+install -m 644 files/driver/bootsplashdriver.py "${ROOTFS_DIR}/displaydriver/bootsplashdriver.py"
+install -m 644 files/driver/shutdowndriver.py "${ROOTFS_DIR}/displaydriver/shutdowndriver.py"
+install -m 644 files/driver/clear.py "${ROOTFS_DIR}/displaydriver/clear.py"
+install -m 644 files/driver/driver.py "${ROOTFS_DIR}/displaydriver/driver.py"
+install -m 644 files/driver/Geist-Regular.ttf "${ROOTFS_DIR}/displaydriver/Geist-Regular.ttf"
+install -m 644 files/driver/GeistMono-Regular.ttf "${ROOTFS_DIR}/displaydriver/GeistMono-Regular.ttf"
 
-install -m 644 files/boot.png "${ROOTFS_DIR}/displaydriver/boot.png"
-install -m 644 files/shutdown.png "${ROOTFS_DIR}/displaydriver/shutdown.png"
+install -m 644 files/driver/boot.png "${ROOTFS_DIR}/displaydriver/boot.png"
+install -m 644 files/driver/shutdown.png "${ROOTFS_DIR}/displaydriver/shutdown.png"
 
-install -m 644 files/splash-boot.service "${ROOTFS_DIR}/etc/systemd/system/splash-boot.service"
+install -m 644 files/driver/splash-boot.service "${ROOTFS_DIR}/etc/systemd/system/splash-boot.service"
 
 on_chroot << EOF
     systemctl enable splash-boot
 EOF
 
-install -m 644 files/splash-shutdown.service "${ROOTFS_DIR}/etc/systemd/system/splash-shutdown.service"
+install -m 644 files/driver/splash-shutdown.service "${ROOTFS_DIR}/etc/systemd/system/splash-shutdown.service"
 
 on_chroot << EOF
     systemctl enable splash-shutdown
 EOF
 
-install -m 644 files/displaydriver.service "${ROOTFS_DIR}/etc/systemd/system/displaydriver.service"
+install -m 644 files/driver/displaydriver.service "${ROOTFS_DIR}/etc/systemd/system/displaydriver.service"
 
 on_chroot << EOF
     systemctl enable displaydriver
